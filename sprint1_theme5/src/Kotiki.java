a45c04e95a4bf29dac47a07a8bee1b568770102e
ReginaFefelova <114951960+ReginaFefelova@users.noreply.github.com>
2022-10-18 21:39:11 +0300

    Update Kotiki.java

--- sprint1_theme5/src/Kotiki.java
+++ sprint1_theme5/src/Kotiki.java
@@ -1,10 +1,12 @@
 public class Kotiki {
     String name;
     int age;
-
-    Kotiki(String name, int age) {
+    String color;
+    
+    Kotiki(String name, int age, String color) {
         this.name = name;
         this.age = age;
+        this.color = color;
     }
 
 }
